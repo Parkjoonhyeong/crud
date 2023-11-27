a2a5510bff2f4a6269b1482b034ab6be7bfa4c40
Joonhyeong Park <pjh010919@gmail.com>
2023-11-27 16:25:25 +0900

    1st

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <h1 className="text-3xl font-bold">WebDev Topics</h1>
       <p className="mb-4">MongoDB CRUD Example</p>
-      {/* <TopicsList /> */}
+      <TopicsList />
     </>
   )
 }
